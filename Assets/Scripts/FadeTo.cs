dcb4c3193ea1323f73c2f30861290392a2bef153
Made Adi Wiradarma <adi.wiradarma423@gmail.com>
2018-01-07 20:19:53 +0700

    Pemasukan script FadeTo

---
+++ Assets/Scripts/FadeTo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeTo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void GoToScene(string sceneName)
+    {
+        Initiate.Fade(sceneName, Color.black, 2.0f);
+    }
+}
