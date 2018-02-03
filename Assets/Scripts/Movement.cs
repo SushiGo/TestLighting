bff0566e91a3bacaa5453d0fb6f7695a2e4d47aa
Albertus Tjahjono <fleksaboy@gmail.com>
2018-02-03 17:18:55 +0700

    Masukin Script Jalan sama Select Obj (WAVE 2)

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public bool CanMove = true;
+    // VR Main Camera
+    private Transform CameraNya;
+    // Speed to move the player
+    public float speed = 3f;
+    // CharacterController script
+    CharacterController myCC;
+
+
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+
+    // Use this for initialization
+    void Start () {
+
+        myCC = gameObject.GetComponent<CharacterController>();
+        CameraNya = Camera.main.transform;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (CanMove)
+        {
+            myCC.SimpleMove(speed * CameraNya.TransformDirection(Vector3.forward * Input.GetAxis("Vertical") + Vector3.right * Input.GetAxis("Horizontal")));
+        }
+    }
+
+    void Update()
+    {
+        if (CanMove)
+        {
+            yaw += speedH * Input.GetAxis("Mouse X");
+            pitch -= speedV * Input.GetAxis("Mouse Y");
+
+            CameraNya.transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+        }
+    }
+}
---
+++ Assets/Scripts/SelectObject.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectObject : MonoBehaviour {
+
+    GameObject Penjelasan_Sekarang;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Mouse is down");
+
+            RaycastHit hitInfo = new RaycastHit();
+            bool hit = Physics.Raycast(Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0.0f)), Camera.main.transform.forward, out hitInfo, 50f);
+            if (hit)
+            {
+                if (hitInfo.transform.gameObject.tag == "obj_pilihan")
+                {
+                    GameObject[] UI_Penjelasan = GameObject.FindGameObjectsWithTag("UI Penjelasan");
+                    int i = 0;
+                    while (UI_Penjelasan[i].name != hitInfo.transform.gameObject.name)
+                    {
+                        i++;
+                    }
+                    if(UI_Penjelasan[i].name == hitInfo.transform.gameObject.name)
+                    {
+                        this.gameObject.GetComponentInParent<Movement>().CanMove = false;
+                        Penjelasan_Sekarang = UI_Penjelasan[i];
+                        UI_Penjelasan[i].transform.localScale = new Vector3(1, 1, 1);
+                    }
+                }
+            }
+            Debug.Log("Mouse is down");
+        }
+        if (Input.GetMouseButtonDown(1) && Penjelasan_Sekarang != null)
+        {
+            this.gameObject.GetComponentInParent<Movement>().CanMove = true;
+            Penjelasan_Sekarang.transform.localScale = new Vector3(0, 0, 0);
+            Penjelasan_Sekarang = null;
+        }
+    }
+}
